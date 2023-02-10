d5a350ec47a16d8c5c5213fc1c79df2973506405
CCBB <CCBB@CCBB-23>
2023-02-10 09:49:18 -0600

    Agregar archivos de proyecto.

---
+++ Prueba123/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Prueba123
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dvgdatos = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btncalc = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbltotal = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgdatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dvgdatos
+            // 
+            this.dvgdatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgdatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.lastname});
+            this.dvgdatos.Location = new System.Drawing.Point(84, 123);
+            this.dvgdatos.Name = "dvgdatos";
+            this.dvgdatos.Size = new System.Drawing.Size(342, 277);
+            this.dvgdatos.TabIndex = 0;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Cuenta";
+            this.name.Name = "name";
+            this.name.Width = 200;
+            // 
+            // lastname
+            // 
+            this.lastname.HeaderText = "Valor";
+            this.lastname.Name = "lastname";
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(215, 406);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(79, 29);
+            this.btnadd.TabIndex = 1;
+            this.btnadd.Text = "Agregar";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(338, 406);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(88, 29);
+            this.btnsave.TabIndex = 3;
+            this.btnsave.Text = "Guardar";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // btncalc
+            // 
+            this.btncalc.Location = new System.Drawing.Point(84, 406);
+            this.btncalc.Name = "btncalc";
+            this.btncalc.Size = new System.Drawing.Size(74, 29);
+            this.btncalc.TabIndex = 4;
+            this.btncalc.Text = "Calcular";
+            this.btncalc.UseVisualStyleBackColor = true;
+            this.btncalc.Click += new System.EventHandler(this.btncalc_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(477, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Total Activo Cirulante";
+            // 
+            // lbltotal
+            // 
+            this.lbltotal.AutoSize = true;
+            this.lbltotal.Location = new System.Drawing.Point(662, 152);
+            this.lbltotal.Name = "lbltotal";
+            this.lbltotal.Size = new System.Drawing.Size(16, 13);
+            this.lbltotal.TabIndex = 6;
+            this.lbltotal.Text = "...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(237, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(293, 31);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "BALANCE GENERAL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbltotal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btncalc);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.dvgdatos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dvgdatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dvgdatos;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastname;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btncalc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbltotal;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Prueba123/Form1.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using static System.Net.WebRequestMethods;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using objExcel = Microsoft.Office.Interop.Excel;
+
+namespace Prueba123
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            string a = Interaction.InputBox("Documentos por cobrar (c/p)", "Registro", "", 200, 200);
+            dvgdatos.Rows.Add("Documentos por cobrar (c/p)", a);
+            string b = Interaction.InputBox("Cuentas por cobrar", "Registro", "", 200, 200);
+            dvgdatos.Rows.Add("Cuentas por cobrar", b);
+            string c = Interaction.InputBox("Estimacion para ctas. Incobrables", "Registro", "", 200, 200);
+            dvgdatos.Rows.Add("Estimacion para ctas. Incobrables", c);
+            string d = Interaction.InputBox("Impuestos pagados por anticipado", "Registro", "", 200, 200);
+            dvgdatos.Rows.Add("Impuestos pagados por anticipado", d);
+            string E = Interaction.InputBox("Acciones y valores", "Registro", "", 200, 200);
+            dvgdatos.Rows.Add("Acciones y valores", E);
+            string f = Interaction.InputBox("Documentos por cobrar (l/p)", "Registro", "", 200, 200);
+            dvgdatos.Rows.Add("Documentos por cobrar (l/p)", f);
+            string g = Interaction.InputBox("Caja y banco", "Registro", "", 200, 200);
+            dvgdatos.Rows.Add("Caja y banco", g);
+            string h = Interaction.InputBox("Inventario", "Registro", "", 200, 200);
+            dvgdatos.Rows.Add("Inventario", h);
+
+
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            objExcel.Application objAplicacion = new objExcel.Application();
+            Workbook objLibro = objAplicacion.Workbooks.Add(XlSheetType.xlWorksheet);
+            Worksheet objHoja = (Worksheet)objAplicacion.ActiveSheet;
+
+            objAplicacion.Visible = false;
+
+
+
+            foreach (DataGridViewColumn columna in dvgdatos.Columns)
+            {
+                objHoja.Cells[1, columna.Index + 1] = columna.HeaderText;
+                foreach (DataGridViewRow fila in dvgdatos.Rows)
+                {
+                    objHoja.Cells[fila.Index + 2, columna.Index + 1] = fila.Cells[columna.Index].Value;
+                }
+            }
+
+            objLibro.SaveAs(ruta + "\\MiprimerExcel.xlsx");
+            objLibro.Close();
+
+        }
+
+        private void btncalc_Click(object sender, EventArgs e)
+        {
+            double suma=0;
+            foreach (DataGridViewRow row in dvgdatos.Rows) 
+            {
+                suma += Convert.ToDouble(row.Cells["lastname"].Value);
+            }
+
+            lbltotal.Text = suma.ToString();
+        }
+    }
+
+}
+
